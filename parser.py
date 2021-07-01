5f093a6199356c73ca4f815875c070197de9cb8a
Edward Li <LegendEddie18@gmail.com>
2021-07-01 16:11:56 -0400

    Add parser

--- parser.py
+++ parser.py
@@ -3,6 +3,18 @@ import re
 import subprocess
 
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[33m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
 def raw(string: str, replace: bool = False) -> str:
     """Returns the raw representation of a string. If replace is true, replace a single backslash's repr \\ with \."""
     r = repr(string)[1:-1]  # Strip the quotes from representation
@@ -14,6 +26,8 @@ def raw(string: str, replace: bool = False) -> str:
 def make_file(file_name, content):
     subprocess.Popen('touch {0}'.format(
         file_name), shell=True, stdout=subprocess.PIPE)
+    subprocess.Popen('> {0}'.format(
+        file_name), shell=True, stdout=subprocess.PIPE)
     subprocess.Popen('echo "{1}" >> {0}'.format(
         file_name, content), shell=True, stdout=subprocess.PIPE)
 
@@ -21,7 +35,7 @@ def make_file(file_name, content):
 LINK = "https://codeforces.com/contest/50/problem/A"
 
 
-def parse_problem(LINK):
+def parse_problem(LINK, path="./"):
     name = ""
     for i in range(len(LINK)-1, -1, -1):
         if(LINK[i] == '/'):
@@ -33,30 +47,71 @@ def parse_problem(LINK):
     inputs = []
     outputs = []
     for i in range(len(all_starts)):
-        print(f.text[all_starts[i]:all_ends[i]])
-    for i in range(len(all_starts)):
         if(i & 1):
             outputs.append((all_starts[i], all_ends[i]))
         else:
             inputs.append((all_starts[i], all_ends[i]))
     current_input_num = 1
     current_output_num = 1
-    for item in inputs:
+    for i in range(len(inputs)):
+        item = inputs[i]
         s = f.text[item[0]:item[1]].replace("<br />", "").replace("<pre>", "")
         if(len(s) and s[0] == '\n'):
             s = s[1:]
         if(len(s) and s[-1] == '\n'):
             s = s[:-1]
-        make_file("{0}.in".format(current_input_num), s)
+        print("Parsing input {0}{1}{2}".format(
+            bcolors.BOLD, current_input_num, bcolors.ENDC))
+        print("=================================")
+        print("{0}{1}{2}".format(bcolors.OKBLUE, s, bcolors.ENDC))
+        print("=================================")
+        make_file("{0}{1}.in".format(path, current_input_num), s)
         current_input_num += 1
-    for item in outputs:
+
+        item = outputs[i]
         s = f.text[item[0]:item[1]].replace("<br />", "").replace("<pre>", "")
         if(len(s) and s[0] == '\n'):
             s = s[1:]
         if(len(s) and s[-1] == '\n'):
             s = s[:-1]
-        make_file("{0}.out".format(current_output_num), s)
+        print("Parsing output {0}{1}{2}".format(
+            bcolors.BOLD, current_output_num, bcolors.ENDC))
+        print("=================================")
+        print("{0}{1}{2}".format(bcolors.OKBLUE, s, bcolors.ENDC))
+        print("=================================")
+        make_file("{0}{1}.out".format(path, current_output_num), s)
         current_output_num += 1
 
 
-parse_problem(LINK)
+def parse_contest(ID):
+    LINK = "https://codeforces.com/contest/"
+    LINK += str(ID)
+    f = requests.get(LINK)
+    search_link = "/contest/{0}/problem/".format(ID)
+    all_starts = [m.start() for m in re.finditer(
+        search_link, f.text)]
+    problems = []
+    for item in all_starts:
+        cur_prob = ""
+        cur_add = 0
+        while(f.text[item+len(search_link)+cur_add] != '"'):
+            cur_prob += f.text[item+len(search_link)+cur_add]
+            cur_add += 1
+        if((len(problems) and problems[-1] == cur_prob) == False):
+            problems.append(cur_prob)
+    for problem in problems:
+        subprocess.Popen('mkdir {0}'.format(problem),
+                         shell=True, stdout=subprocess.PIPE)
+        subprocess.Popen('touch {0}/{1}.cpp'.format(
+            problem, problem.lower()), shell=True, stdout=subprocess.PIPE)
+        problem_link = "https://codeforces.com/contest/{0}/problem/{1}".format(
+            ID, problem)
+        print("{0}Parsing problem {1}{2}{3}".format(bcolors.HEADER,
+                                                    bcolors.BOLD, problem, bcolors.ENDC))
+        parse_problem(problem_link, "{0}/".format(problem))
+    print("{0}Finished parsing contest{1}".format(
+        bcolors.OKGREEN, bcolors.ENDC))
+
+
+    # parse_problem(LINK)
+parse_contest("1533")
